d9f84e8a4c68e0b65ea691bd57211c3e49fb6b2a
Paulo Raimundo <paulo.raimundo@mycapital.com.br>
2022-10-07 13:52:17 -0300

    aula 21: Api Spring: Criar Curso (HTTP POST)

--- src/main/java/com/paulo/controller/CourseController.java
+++ src/main/java/com/paulo/controller/CourseController.java
@@ -5,8 +5,13 @@ import com.paulo.repository.CourseRepository;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,8 +24,16 @@ public class CourseController {
         this.courseRepository = courseRepository;
     }
     
+    // @RequestMapping(method = RequestMethod.GET)
     @GetMapping
-    public List<Course> list() {
+    public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    // @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
+    public ResponseEntity<Course> create(@RequestBody Course course) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(null);
     }
 }
